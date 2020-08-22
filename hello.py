82c492539d82154783419fce02387e856aea808a
cheata96 <63210327+cheata96@users.noreply.github.com>
2020-08-22 16:16:34 -0500

    new file

---
+++ hello.py
@@ -0,0 +1,15 @@
+# This program says hellow and asks for my name
+
+print(' Hey girl!')
+
+print('What is your name?') # ask for their name
+myName = input ()
+
+print('It is good to meet ya, ' + myName)
+
+print('The length of your name is:')
+print(len(myName) )
+
+print('How old are you?') # ask for their age
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
